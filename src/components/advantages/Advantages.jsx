47218f3dcf3d795434199cdedea9b62a861c33ee
Roman <generallevy45@mail.ru>
2023-02-13 16:30:50 +0300

    add slider and advantages

---
+++ src/components/advantages/Advantages.jsx
@@ -0,0 +1,47 @@
+import './advantages.scss'
+import lowPrice from '../../assets/advantages/lowPrice.png'
+import delivery from '../../assets/advantages/delivery.png'
+import certificates from '../../assets/advantages/certificates.png'
+import partners from '../../assets/advantages/partners.png'
+
+export const arrAdvantages = [
+  {
+    id: 1,
+    text: 'Впечатления по цене партнера',
+    img: lowPrice
+  },
+  {
+    id: 2,
+    text: 'Бесплатная доставка в день заказа',
+    img: delivery
+  },
+  {
+    id: 3,
+    text: 'Красивые сертификаты на любое впечатление',
+    img: certificates
+  },
+  {
+    id: 4,
+    text: 'Проверенные партнёры',
+    img: partners
+  }
+]
+
+const Advantages = () => {
+  return (
+    <>
+      <div className="advantages container">
+        {
+          arrAdvantages.map(item => (
+            <div className="advantages__item">
+              <img src={item.img} alt="advantages img" height="80"/>
+              <p>{item.text}</p>
+            </div>
+          ))
+        }
+      </div>
+    </>
+  )
+}
+
+export default Advantages
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,11 @@
 import Slider from '../components/slider/Slider'
+import Advantages from '../components/advantages/Advantages'
 
 const HomePage = () => {
   return (
     <>
       <Slider/>
+      <Advantages/>
     </>
   )
 }
