bab972b9791cc64d75a00dd4037d29fbfec4132b
Kaori318 <83631704+Kaori318@users.noreply.github.com>
2023-02-15 19:36:17 +0600

    1

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,15 @@
 import Slider from '../components/slider/Slider'
 import Advantages from '../components/advantages/Advantages'
+import CatalogImpressions from '../components/catalogImpressions/CatalogImpressions'
+
+
 
 const HomePage = () => {
   return (
     <>
       <Slider/>
       <Advantages/>
+      <CatalogImpressions/>
     </>
   )
 }
