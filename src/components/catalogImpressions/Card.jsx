b4353955e66e213d6d84b1f65f59b77dc36d41c8
Kaori318 <83631704+Kaori318@users.noreply.github.com>
2023-02-16 17:08:04 +0600

    card modal
    card modal

--- src/components/catalogImpressions/Card.jsx
+++ src/components/catalogImpressions/Card.jsx
@@ -1,35 +1,24 @@
 import React from 'react';
 import './CatalogImpressions.scss'
-// import {CardButton} from './UI/button/CardButton';
-import { Modal } from '../modal/modal'
-import { useState } from 'react'
-import Details from '../details/Details';
-// import { Details } from '../details/Details';
 
-const Card = ({ card, modal }) => {
 
-  const [modalActive, setModalActive] = useState(false)
+const Card = ({ card, modal1, modal2 }) => {
 
   return (
-    <div className="card" onClick={() => modal(true)}>
-      <div className="card__img">
+    <div className="card">
+      <div className="card__img" onClick={() => modal2(true)}>
         <img
           src={card.img}
           alt={card.title} />
       </div>
       <div className="card__block">
-        <div className="card__title">{card.name}</div>
+        <div className="card__title"  onClick={() => modal2(true)}>{card.name}</div>
         <div className="card__body">
           <h1 className='price'>{card.price}</h1>
-          <button className="cardBtn" onClick={() => modal(true)}>Заказать</button>
-          <p onClick={() => setModalActive(true)}>Подробнее</p>
+          <button className="cardBtn" onClick={() => modal1(true)}>Заказать</button>
+          <p  onClick={() => modal2(true)}>Подробнее</p>
         </div>
       </div>
-      <Modal active={modalActive} setActive={setModalActive}>
-        <Details />
-      </Modal>
-
-
     </div>
   );
 };
