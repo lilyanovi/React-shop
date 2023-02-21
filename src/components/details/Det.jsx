17028db1529b5eed95240c6cf2118c8482ed6209
Kaori318 <83631704+Kaori318@users.noreply.github.com>
2023-02-21 18:16:42 +0600

    modalCard

--- src/components/details/Det.jsx
+++ src/components/details/Det.jsx
@@ -1,18 +1,36 @@
 import React from 'react';
 import style from './details.module.css'
+import { Modal } from '../modal/modal'
+import { Application } from '../application/application'
+import { useState } from 'react'
 
-const Det = ({card}) => {
+const Det = ({card, modal1, modal2}) => {
+  const [modalActive, setModalActive] = useState(false)
+
  return (
+  <>
   <form className={style.details}>
-                        <div className={style.details__head}>
+  <div style={{display:'flex'}}>
+    <div className={style.images}>
+    <img src={card.img} alt="img"/>
+    </div>
+    <div className={style.details__head}>
                             <h2 className={style.details__head__title} >{card.name}</h2>
                             <h1 className={style.details__price} >{card.price}</h1>
                         </div>
-                        <button className={style.details__btn}>
+                        <button className={style.details__btn}
+                        onClick={()=> setModalActive(true)}>
+                      
                           Заказать впечатление</button>
-                        <img src={card.img} alt="img" />
+                       
                         <p> Тут будет добавлена подробная информация </p>
-                    </form>
+               
+  </div>
+  </form>
+  <Modal active={modalActive} setActive={setModalActive}>
+  <Application />
+</Modal>
+  </>
  );
 };
 export default Det;
