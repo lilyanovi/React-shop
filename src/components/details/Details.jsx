981e4e930202216d97cc16e79b96ffdbda11d5b0
JuliaKyz <Julie.abukhova@gmail.com>
2023-03-06 00:52:07 +0300

    added image slider to details

--- src/components/details/Details.jsx
+++ src/components/details/Details.jsx
@@ -1,4 +1,5 @@
 import style from './details.module.scss'
+import React, { useState } from 'react'
 //import horseWalk from '../../assets/slider/horseWalk.png'
 import { Modal } from '../modal/modal'
 import { Application } from '../application/application'
@@ -15,7 +16,7 @@ import { openModal, closeModalDetail } from '../../store/modal/actions'
 // ]
 
 
-const Details = ({card}) => {
+const Details = ({ card }) => {
 
     const modalShow = useSelector(store => store.modal.modalShow)
     const dispatch = useDispatch()
@@ -23,39 +24,62 @@ const Details = ({card}) => {
         event.preventDefault()
         dispatch(openModal(true))
         dispatch(closeModalDetail(false))
+    };
+
+    const imgs = [
+        { id: 0, value: "https://wallpaperaccess.com/full/2637581.jpg" },
+        { id: 1, value: "https://source.unsplash.com/user/c_v_r/1900x800" },
+        { id: 2, value: "https://source.unsplash.com/user/c_v_r/100x100" },
+    ]
+    const [wordData, setWordData] = useState(imgs[0])
+    const handleClick = (index) => {
+        console.log(index)
+        const wordSlider = imgs[index];
+        setWordData(wordSlider)
     }
 
     return (
-       <>
-        <div className={style.details} key={card.id}>
-            <div className={style.details__images}>
-                <img className={style.details__images_img} src={card.img} alt="img" />
-                <div className={style.details__images_mini}>
-                    <img className={style.details__images_img_mini} src={card.img} alt="img" />
-                    <img className={style.details__images_img_mini} src={card.img} alt="img" />
-                    <img className={style.details__images_img_mini} src={card.img} alt="img" />
+        <>
+            <div className={style.details} key={card.id}>
+                <div className={style.details__images}>
+                    <img className={style.details__images_img} src={wordData.value} alt="img" />
+
+                    <div className='flex_row'>
+                        {imgs.map((data, i) =>
+                            <div className={style.details__images_mini} key={i} >
+                                <img className={style.details__images_img_mini} src={data.value} alt="img" onClick={() => handleClick(i)} />
+                            </div>
+                        )}
+                    </div>
+
+
+                    {/* <img className={style.details__images_img_mini} src={card.img} alt="img" />
+                        <img className={style.details__images_img_mini} src={card.img} alt="img" />
+                        <img className={style.details__images_img_mini} src={card.img} alt="img" /> */}
+
+
+
+                </div>
+                <div className={style.details__body}>
+                    <h2 className={style.details__body_title} >{card.name.toUpperCase()}</h2>
+                    <div className={style.details__selectionPrice}>
+                        <h1 className={style.details__price} >{card.price} ₽</h1>
+                        {/* <button className={style.details__selection}>Менять описание</button> */}
+                    </div>
+                    <button className={style.details__btn} onClick={(event) => handleOpenModal(event)}>Заказать впечатление</button>
+                    <div className={style.details__description}>
+                        <h3>Подробнее о впечатлении: </h3>
+                        {card.desc}
+                    </div>
                 </div>
             </div>
-            <div className={style.details__body}>
-                <h2 className={style.details__body_title} >{card.name.toUpperCase()}</h2>
-                <div className={style.details__selectionPrice}>
-                    <h1 className={style.details__price} >{card.price} ₽</h1>
-                    {/* <button className={style.details__selection}>Менять описание</button> */}
-                </div>
-                <button className={style.details__btn} onClick={(event) => handleOpenModal(event)}>Заказать впечатление</button>
-                <div className={style.details__description}>
-                    <h3>Подробнее о впечатлении: </h3>
-                    {card.desc}
-                </div>
-            </div>
-        </div>
-        {
-        modalShow &&
-        <Modal>
-            <Application />
-        </Modal>
-        }
-    </>
+            {
+                modalShow &&
+                <Modal>
+                    <Application />
+                </Modal>
+            }
+        </>
     )
 }
 
