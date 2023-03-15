4a2f46983ca9f79ecae160bbe63b8b22671ae3fc
Evgeniy Ippolitov <ippevg@yandex.ru>
2023-03-15 14:42:01 +0300

    RoutingOnLoadSite&LayoutFilters
    Поправил роутинг при начальной загрузке страницы и переделал внешний вид фильтра по  новому макету. Сделал фильтр открывающимся по кнопке, как предложила Анастасия. Если не понравится - уберу это.

--- src/components/filterCost/filterCost.jsx
+++ src/components/filterCost/filterCost.jsx
@@ -6,7 +6,7 @@ export function FilterCost(props) {
     const [min, setMin] = useState('');
     const [max, setMax] = useState('');
     const [value, setVal] = useState('');
-
+    const [filter, setFilter] = useState(false);
     function changeMin(event) {
         if (+event.target.value >= max) {
             setMin(+event.target.value);
@@ -36,33 +36,40 @@ export function FilterCost(props) {
 
     return (
         <>
-            <div className='filterBox'>
-                <h5>Сортировать по цене</h5>
-                <div className='boxForm'>
-                    <div className='radioBox'>
-                        <label htmlFor="radioDown">
-                            <input id="radioDown" className='radio' type="radio" name="radio" value="1"
-                                checked={value === '1' ? true : false}
-                                onChange={sortImpressions}
-                            />
-                            <span> сначала дешевые </span>
-                        </label>
-                        <label htmlFor="radioUp" >
-                            <input id="radioUp" type="radio" className='radio' name="radio" value="2"
-                                checked={value === '2' ? true : false}
-                                onChange={sortImpressions} />
-                            <span> сначала догорие </span>
-                        </label>
+            <div className="filterWrapper">
+                {filter ? <div className='filterBox'>
+                    <h5 className='filters' onClick={() => setFilter(false)}>Сортировать по цене &#9650;</h5>
+                    <div className='boxForm'>
+                        <div className='radioBox'>
+                            <label htmlFor="radioDown">
+                                <input id="radioDown" className='radio' type="radio" name="radio" value="1"
+                                    checked={value === '1' ? true : false}
+                                    onChange={sortImpressions}
+                                />
+                                <span> Сначала дешевые </span>
+                            </label>
+                            <label htmlFor="radioUp" >
+                                <input id="radioUp" type="radio" className='radio' name="radio" value="2"
+                                    checked={value === '2' ? true : false}
+                                    onChange={sortImpressions} />
+                                <span> Сначала догорие </span>
+                            </label>
+                        </div>
+                        <div className='inputsBox'>
+                            <span > от </span>
+                            <input type="number" step={100} id='minCost' min="0" value={min} onChange={changeMin} className='inputFilter min' />
+                            <span className='prefix'> до </span>
+                            <input type="number" step={100} id='maxCost' min="0" value={max} onChange={changeMax} className='inputFilter max' />
+                            <span > &emsp;  &ensp;</span><button onClick={() => props.watchChange(min, max, value)} className='submit' >Применить</button>
+                            <span className='prefix'> &emsp; &ensp;</span>  <button onClick={resetFilter} className='submit' >Сбросить</button>
+                        </div>
                     </div>
-                    <div className='inputsBox'>
-                        <input type="number" step={100} id='minCost' min="0" value={min} onChange={changeMin} className='inputFilter min' />
-                        <input type="number" step={100} id='maxCost' min="0" value={max} onChange={changeMax} className='inputFilter max' />
-                    </div>
-                    <button onClick={() => props.watchChange(min, max, value)} className='submit' >Применить</button>
-                    <button onClick={resetFilter} className='submit' >Сбросить</button>
-                </div>
+
+                </div> : <h5 className='filters' onClick={() => setFilter(true)}>Фильтры &#9660;</h5>}
+
 
             </div>
+
         </>
     )
 }
