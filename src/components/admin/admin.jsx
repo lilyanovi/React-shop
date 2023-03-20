77b4ddc4cb9a38ebcee9e0add53f8a3127282d48
Okscreator <okscreator@gmail.com>
2023-03-21 00:30:33 +0300

    lkAmin
    lkAmin

--- src/components/admin/admin.jsx
+++ src/components/admin/admin.jsx
@@ -1,42 +1,41 @@
 import { NavLink, Outlet, useNavigate } from "react-router-dom"
+import React, { useState } from 'react';
+import './admin.scss'
 import { useDispatch } from "react-redux"
 import { signOut, getAuth } from "firebase/auth"
 import { removeUser } from "../../store/auth/action"
 
-
 const Admin = () => {
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const auth = getAuth();
 
-    const dispatch = useDispatch()
-    const navigate = useNavigate()
-    const auth = getAuth();
-
-    const handleLogOut = event => {
-        event.preventDefault()
-    
-        signOut(auth)
-          .then(() => {
-            dispatch(removeUser())
-            localStorage.removeItem('remember')
-            navigate('/login')
-          }).catch(error => {
-            console.log(error)
-          })
-      }
-
-    return (
-      <>
-        <section className="container">
-            <p>Admin</p>
-            <button onClick={(e) => handleLogOut(e)}>Выход</button>
-            <nav>
-                <NavLink to="/admin">Управление заявками</NavLink>
-                <NavLink to="/admin/subscriptions">Управление подписками</NavLink>
-                <NavLink to="/admin/comments">Управление отзывами</NavLink>
-            </nav>
-            <Outlet/>
-        </section>
-      </>
-    )
+  const handleLogOut = event => {
+    event.preventDefault()
+    signOut(auth)
+      .then(() => {
+        dispatch(removeUser())
+        localStorage.removeItem('remember')
+        navigate('/login')
+      }).catch(error => {
+        console.log(error)
+      })
   }
 
-  export default Admin
+  return (
+    <>
+      <section className="admin container">
+        <h1>Администрирование</h1>
+        <nav>
+          <NavLink className="admin-nav" to="/admin">Управление заявками</NavLink>
+          <NavLink className="admin-nav" to="/admin/comments">Управление отзывами</NavLink>
+          <NavLink className="admin-nav" to="/admin/subscriptions">Управление подписками</NavLink>
+          <NavLink className="admin-nav" onClick={(e) => handleLogOut(e)}>Выход</NavLink>
+        </nav>
+        <Outlet />
+      </section>
+    </>
+  )
+}
+
+export default Admin
--- src/components/admin/applicationManagement/applicationManagement.jsx
+++ src/components/admin/applicationManagement/applicationManagement.jsx
@@ -1,8 +1,82 @@
+import { getApplicationList, writeUserApplicationStatus, writeApplicationStatusAdmin } from '../../../services/firebase'
+import { useState, useEffect } from 'react'
+import './applicationManagement.scss'
+
 const ApplicationManagementAdmin = () => {
+  const [list, setList] = useState({})
+  const [setFilter] = useState(false);
+  const [value] = useState('');
+
+  useEffect(() => {
+    getApplicationList()
+      .then(data => {
+        setList(data)
+      })
+  }, [])
+
+  console.log(list)
 
   return (
     <>
-      <p>ApplicationManagement</p>
+      <div className='applicationManagmentAdminFilter'>
+        <div className='applicationManagmentAdminFilter__title' onClick={() => setFilter(false)}>Сортировать по цене:</div>
+        <div className='applicationManagmentAdminFilter__radioBox'>
+          <label htmlFor="radioDown">
+            <input id="radioDown" className='radio' type="radio" name="radio" value="1"
+              checked={value === '1' ? true : false} />
+            <span>дате</span>
+          </label>
+        </div>
+        <div className='applicationManagmentAdminFilter__radioBox'>
+          <label htmlFor="radioUp" >
+            <input id="radioUp" type="radio" className='radio' name="radio" value="2"
+              checked={value === '2' ? true : false} />
+            <span>статусу</span>
+          </label>
+        </div>
+      </div>
+      <div className="applicationManagmentAdmin">
+        {Object.keys(list).map(key => (
+          <div className="applicationManagmentAdmin__item" key={key}>
+            <div className="applicationManagmentAdmin__item_iner">
+              <div className="applicationManagmentAdmin__item_iner-title">№ заказа</div>
+              <div className="applicationManagmentAdmin__item_iner-key">{key}</div>
+            </div>
+            <div className="applicationManagmentAdmin__item_iner">
+              <div className="applicationManagmentAdmin__item_iner-title">Дата</div>
+              <div className="applicationManagmentAdmin__item_iner-key">{list[key].date}</div>
+            </div>
+            <div className="applicationManagmentAdmin__item_iner">
+              <div className="applicationManagmentAdmin__item_iner-title">Название</div>
+              <div className="applicationManagmentAdmin__item_iner-key">Название</div>
+            </div>
+            <div className="applicationManagmentAdmin__item_iner">
+              <div className="applicationManagmentAdmin__item_iner-title">Id пользователя</div>
+              <div className="applicationManagmentAdmin__item_iner-key">{list[key].userId}</div>
+            </div>
+            <div className="applicationManagmentAdmin__item_iner">
+              <div className="applicationManagmentAdmin__item_iner-title">Имя</div>
+              <div className="applicationManagmentAdmin__item_iner-key">{list[key].name}</div>
+            </div>
+            <div className="applicationManagmentAdmin__item_iner">
+              <div className="applicationManagmentAdmin__item_iner-title">Телефон</div>
+              <div className="applicationManagmentAdmin__item_iner-key">{list[key].phone}</div>
+            </div>
+            <div className="applicationManagmentAdmin__item_iner">
+              <div className="applicationManagmentAdmin__item_iner-title">E-mail</div>
+              <div className="applicationManagmentAdmin__item_iner-key">{list[key].email}</div>
+            </div>
+            <div className="applicationManagmentAdmin__item_iner">
+              <div className="applicationManagmentAdmin__item_iner-title">Статус</div>
+              <div className="applicationManagmentAdmin__item_iner-key">status</div>
+            </div>
+            <div className="applicationManagmentAdmin__item_iner">
+              <div className="applicationManagmentAdmin__item_iner-title">Комментарий</div>
+              <div className="applicationManagmentAdmin__item_iner-key">comment</div>
+            </div>
+          </div>
+        ))}
+      </div>
     </>
   )
 }
