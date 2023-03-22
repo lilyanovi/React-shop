fdcdb26c79018b2b7290fef298eb578c8eb2114b
Roman <generallevy45@mail.ru>
2023-03-22 18:39:48 +0300

    edit modal window

--- src/components/questionModal/QuestionModalWindow.jsx
+++ src/components/questionModal/QuestionModalWindow.jsx
@@ -4,15 +4,15 @@ import './QuestionModalWindow.scss';
 export default function QuestionModalWindow () {
 
   return (
-      <div className = 'background'>
-          <div className = 'modal'>
-            <div className = 'blockText'>
-            <p className = 'text'>Вы уверены, что хотите</p>
-            <p className = 'boldText'>отменить заявку?</p>
+      <div className = 'questionModalWindow__background'>
+          <div className = 'questionModalWindow__modal'>
+            <div className = 'questionModalWindow__blockText'>
+            <p className = 'questionModalWindow__text'>Вы уверены, что хотите</p>
+            <p className = 'questionModalWindow__boldText'>отменить заявку?</p>
             </div>
-            <div className = 'button_container'>
-              <div className = 'button'>Не отменять</div>
-              <div className = 'button'>Отменить</div>
+            <div className = 'questionModalWindow__button_container'>
+              <div className = 'questionModalWindow__button'>Не отменять</div>
+              <div className = 'questionModalWindow__button'>Отменить</div>
             </div>
           </div>
       </div>
