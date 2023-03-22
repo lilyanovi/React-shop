fe131ae09c90dc3e6d7bf804a888560fffdfb4ac
Kaori318 <Kaori.vp@yandex.ru>
2023-03-22 19:11:28 +0300

    details

--- src/components/details/Details.jsx
+++ src/components/details/Details.jsx
@@ -9,6 +9,17 @@ import { selectCard } from '../../store/card/actions'
 
 const Details = ({ card }) => {
 
+    const [wordData, setWordData] = useState(card.imgs[0])
+    const [prices, setPrices] = useState(card.prices)
+    const [selectedMesurement, setSelectedMesurement] = useState(prices[0].price);
+
+    //const [desc1, setDesc1] =useState(Object.values(card.desc)[0]) Если надо чтоб менялось описание  "desc":{"900":"Описание1", "1000": "Описание2"}
+
+    // useEffect(() => {      поменять описание при смене цены
+    //     const index = prices.findIndex(el => el.price === selectedMesurement)
+    //     setDesc1(Object.values(card.desc)[index])
+    // },[selectedMesurement])
+
     const modalShow = useSelector(store => store.modal.modalShow)
     const dispatch = useDispatch()
 
@@ -19,43 +30,55 @@ const Details = ({ card }) => {
         dispatch(closeModalDetail(false))
     };
 
-    const [desc, setDesc] = useState(card.desc)
-    // const imgs = [
-    //     { id: 0, value: "https://wallpaperaccess.com/full/2637581.jpg" },
-    //     { id: 1, value: "https://source.unsplash.com/user/c_v_r/1900x800" },
-    //     { id: 2, value: "https://source.unsplash.com/user/c_v_r/100x100" },
-    // ]
-    const [wordData, setWordData] = useState(card.imgs[0])
+    
     const handleClick = (index) => {
         const wordSlider = card.imgs[index];
         setWordData(wordSlider)
     }
-    //const [desc, setDesc] = useState(card.desc)
-
 
     return (
-
     <>
         <div className={style.details} key={card.id}>
             <div className={style.details__images}>
                 <img className={style.details__images_img} src={wordData.value} alt="img" />
                 <div className={style.flex_row}>
                     {card.imgs.map((data, i) =>
-                        <img key={i} className={style.details__images_img_mini} src={data.value} alt="img" onClick={() => handleClick(i)} />
-                            )}
+                        <img key={i}
+                        className={style.details__images_img_mini}
+                        src={data.value} alt="img"
+                        onClick={() => handleClick(i)}/>
+                    )}
                 </div>
             </div>
        
             <div className={style.details__body}>
-                <h2 className={style.details__body_title} >{card.name.toUpperCase()}</h2>
-                <div className={style.details__selectionPrice}>
-                    <h1 className={style.details__price} >{card.price} ₽</h1>
-                    {/* <button className={style.details__selection}>Менять описание</button> */}
+                <h2 className={style.details__body_title} >
+                    {card.name.toUpperCase()}
+                </h2>
+                <div className={style.details__body_info}>
+                    <h1 className={style.details__price} >
+                    {selectedMesurement} ₽
+                    </h1>
+                    {prices.length>1
+                        ?<div className={style.dropdownList}>
+                            <select className={style.dropdownListItem}
+                                value={selectedMesurement}
+                                onChange={e =>setSelectedMesurement(e.target.value)}
+                            >
+                                {prices.map(p => <option key={p.id} value={p.price}>{p.mesurement}</option>)}
+                            </select>
+                        </div>
+                        : <p style={{visibility: 'hidden'}}></p>
+                    }   
                 </div>
-                <button className={style.details__btn} onClick={(event) => handleOpenModal(event)}>Заказать впечатление</button>
+                <button className={style.details__btn} onClick={(event) => handleOpenModal(event)}>
+                    Заказать впечатление
+                </button>
                 <div className={style.details__description}>
                     <h3>Подробнее о впечатлении: </h3>
-                    <div className={style.details__text}>{desc.map((desc, index) => <p key={index}>{desc}</p>)}</div>
+                    <div className={style.details__text}>
+                        {card.desc.map((desc, index) => <p key={index}>{desc}</p>)}
+                    </div>
                 </div>
             </div>
         </div>
